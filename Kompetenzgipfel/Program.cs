90ac48dc461d103887890ce31b146f3ebb6e1cee
Frederik Schlüter <f.schlueter@posteo.de>
2024-03-07 17:17:28 -0300

    fix(deploy,Program.cs,JWT): potential workaround for bearer token getting refused because of invalid audience when used in conjuction with reverse proxy

--- Kompetenzgipfel/Program.cs
+++ Kompetenzgipfel/Program.cs
@@ -3,21 +3,27 @@ using Kompetenzgipfel;
 using Kompetenzgipfel.Models;
 using Kompetenzgipfel.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 // todo: make sure that .env is the only configuration source. Needs to set the configuration provider for builder but don't know how to do that right know
 DotEnv.Load(Path.Combine(Directory.GetCurrentDirectory(), ".env"));
+if (!builder.Environment.IsDevelopment()) Console.WriteLine("this is in production");
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 builder.WebHost.ConfigureKestrel(options =>
 {
-    var serverPort = int.Parse(Environment.GetEnvironmentVariable("SERVER_PORT")??"8080");
+    var serverPort = int.Parse(Environment.GetEnvironmentVariable("SERVER_PORT") ?? "8080");
     if (builder.Environment.IsDevelopment())
         options.ListenLocalhost(serverPort, listenOptions => listenOptions.UseHttps());
     else
         options.ListenAnyIP(serverPort);
 });
-Console.WriteLine("this is in production");
 builder.Services.AddAuthentication(opt =>
     {
         opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -29,13 +35,16 @@ builder.Services.AddAuthentication(opt =>
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
-            ValidateAudience = true,
+            //todo: when internet is back, set to true and make release + development pass
+            ValidateAudience = false,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
             ValidIssuer = httpPrefix + Environment.GetEnvironmentVariable("IP_ADDRESS") + ":" +
                           Environment.GetEnvironmentVariable("SERVER_PORT"),
-            ValidAudience = httpPrefix + Environment.GetEnvironmentVariable("IP_ADDRESS") + ":" +
-                            Environment.GetEnvironmentVariable("CLIENT_PORT"),
+            // todo: not working with reverse proxy! Will always be https?? + also got different ports. Wildcard? Should not be a security issue
+            // ValidAudience = httpPrefix + Environment.GetEnvironmentVariable("IP_ADDRESS") + ":" +
+            // Environment.GetEnvironmentVariable("CLIENT_PORT")
+            // ,
             IssuerSigningKey =
                 new SymmetricSecurityKey(
                     Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("ENCRYPTION_KEY_JWT_PRIVATE")))
@@ -61,6 +70,7 @@ builder.Services.AddScoped<ITopicService, TopicService>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddSingleton<JwtGenerationService>();
 var app = builder.Build();
+app.UseForwardedHeaders();
 if (!app.Environment.IsDevelopment())
 {
     // Configure the HTTP request pipeline.
