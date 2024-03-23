1969fae6774f906eb4b16e791bbd60f3fa4dc93e
Frederik Schlüter <f.schlueter@posteo.de>
2024-03-23 12:38:35 -0300

    tmp: prepare nginx static host

--- Kompetenzgipfel/ClientApp/src/router/index.ts
+++ Kompetenzgipfel/ClientApp/src/router/index.ts
@@ -13,12 +13,6 @@ const router = createRouter({
             meta: {requiresAuth: false}
         },
         {
-            path: '/index.html',
-            name: 'homeByIndex',
-            component: HomeView,
-            meta: {requiresAuth: false}
-        },
-        {
             path: '/about',
             name: 'about',
             component: () => import('../views/AboutView.vue'),
