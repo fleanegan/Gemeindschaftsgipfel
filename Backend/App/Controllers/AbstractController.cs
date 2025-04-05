4fbfc1ce8d9fff498568d7cb3bf73f478564fb4f
Frederik Schlüter <frederik.schlueter@xitaso.com>
2025-04-05 19:40:52 +0200

    feat: Update dependencies

--- Backend/App/Controllers/AbstractController.cs
+++ Backend/App/Controllers/AbstractController.cs
@@ -1,7 +1,7 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Gemeinschaftsgipfel.Controllers.Helpers;
+namespace Gemeinschaftsgipfel.Controllers;
 
 public abstract class AbstractController : Controller
 {
